83f2dcb674a67f0155c7854e6d8e6d69773b02ab
Ojukwu Onyema <eojukwu244@gmail.com>
2020-01-12 15:31:24 +0100

    commit for lab 5

--- myscrumy/myscrumy/settings.py
+++ myscrumy/myscrumy/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'myscrumy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'linuxjobber_database',
+        'USER': 'linuxjobber',
+        'PASSWORD': 'linuxjobber',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
